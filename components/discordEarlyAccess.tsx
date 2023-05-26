8c7ad614a91107e9a481fe6db27ad5ca02a484af
Bastien Faivre <faivre.bast@gmail.com>
2023-05-26 23:32:14 +0200

    fix: import

--- components/discordEarlyAccess.tsx
+++ components/discordEarlyAccess.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Scene3d from "./scene3d";
-import { CubeDiscord, Model } from "./Cube";
+// import { CubeDiscord, Model } from "./Cube";
 import { styled } from "@mui/system";
 const InfoSection = styled(Box)(({ theme }) => ({
   padding: theme.spacing(4),
@@ -43,7 +43,7 @@ export const DiscordEarlyAccess = () => {
             </span>
           </Typography>
         </Box>
-        <Scene3d asset={<CubeDiscord />} />
+        {/* <Scene3d asset={<CubeDiscord />} /> */}
       </InfoSection>
     </Box>
   );
