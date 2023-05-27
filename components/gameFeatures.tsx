196249cc192e09e458a412322befc50f5eee82c2
U <nils.del@protonmail.com>
2023-05-28 00:07:08 +0200

    fix: temporary images until models are made

--- components/gameFeatures.tsx
+++ components/gameFeatures.tsx
@@ -2,9 +2,49 @@ import React from "react";
 import { Model } from "./Cube";
 import Scene3d from "./scene3d";
 import { Box, Typography, Grid } from "@mui/material";
+import { ModelStarknet } from "./Starknet_cube_24_color_clean";
+import Image from "next/image";
 
+
+// TODO: make models and use this snippet
+// const FeaturesDescription = (props: {
+//   model: JSX.Element;
+//   title: string;
+//   description: string;
+// }) => (
+//   <Box
+//     component={"div"}
+//     sx={{
+//       display: "flex",
+//       flexDirection: "column",
+//       justifyContent: "center",
+//       alignItems: "center",
+//     }}
+//   >
+//     <Scene3d asset={props.model} />
+//     <Typography
+//       gutterBottom
+//       variant="h6"
+//       sx={{
+//         fontWeight: "bold",
+//         color: "#1faa00",
+//         width: "250px",
+//       }}
+//     >
+//       {props.title}
+//     </Typography>
+//     <Typography
+//       // should do exactly 3 lines, not less not more
+//       sx={{ height: "80px", overflow: "hidden", width: "250px" }}
+//     >
+//       {props.description}
+//     </Typography>
+//   </Box>
+// );
+
+// use this while we don't have models
 const FeaturesDescription = (props: {
-  model: JSX.Element;
+  image: string;
   title: string;
   description: string;
 }) => (
@@ -17,7 +57,7 @@ const FeaturesDescription = (props: {
       alignItems: "center",
     }}
   >
-    <Scene3d asset={props.model} />
+    <Image src={props.image} alt="feature image" width={100} height={100} />
     <Typography
       gutterBottom
       variant="h6"
@@ -42,23 +82,27 @@ export const GameFeatures = () => {
   const features = [
     {
       title: "Powered by Starknet",
-      model: <Model />,
+      // model: <ModelStarknet position={[0, 0, 1]} />,
+      image: "/SN-gradient-dark.png",
       description: "Lowest fees for trading, transferring, and owning assets.",
     },
     {
       title: "Massively Multiplayer",
-      model: <Model />,
+      // model: <Model />,
+      image: "/online.png",
       description:
         "Up to 5,000 players on a shared server, all focused on the Metacube.",
     },
     {
       title: "Free 2 Play",
-      model: <Model />,
+      // model: <Model />,
+      image: "/f2p.webp",
       description: "No integrated purchases, completely free to play.",
     },
     {
       title: "One-Time Event",
-      model: <Model />,
+      // model: <Model />,
+      image: "/clock.webp",
       description:
         "Join the unique Metacube destruction and await the next season's mysteries.",
     },
@@ -82,7 +126,8 @@ export const GameFeatures = () => {
             <Grid key={index} item xs={12} md={6} lg={3}>
               <FeaturesDescription
                 title={feature.title}
-                model={feature.model}
+                // model={feature.model}
+                image={feature.image}
                 description={feature.description}
               />
             </Grid>
