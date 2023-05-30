16dcf545dc660061f6925c627036abe204f36058
KamyarTaher <61198701+KamyarTaher@users.noreply.github.com>
2023-05-30 10:36:02 +0200

    hero fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -44,11 +44,17 @@ const Image = styled("img")({
   objectPosition: "top", // this will align the image to the top of the box
   width: "500px", // change this to the desired width
   height: "500px", // change this to the desired height
+  // reduce image size on mobile
+  "@media (max-width: 600px)": {
+    width: "300px",
+    height: "300px",
+  },
 });
 
 const Image1 = styled(Image)({
   animation: `${fadeIn} 2s, ${slideRight} 2s`,
   // when out of the screen, it shouldn't extend the page width
+  objectPosition: "top right",
 });
 
 const Image2 = styled(Image)({
