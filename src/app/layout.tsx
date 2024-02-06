f6da95829d57b9f882bd81c6409f12a4a93d25e3
NilsDelage <nils.del@protonmail.com>
2024-02-06 22:05:31 +0100

    fix: metadata images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,12 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Metacube | Free2Play on Starknet',
   description: 'Metacube is a Massive Multiplayer Free2Play Event game on Starknet. Compete with thousands of players, own the assets you collect, and conquer the Metacube.',
+  openGraph: {
+    images: '/metadata-image.jpeg'
+  },
+  twitter: {
+    images: '/metadata-image.jpeg'
+  },
 }
 
 export default function RootLayout({
