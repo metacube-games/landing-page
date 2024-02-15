1969402fd0a73e0f69b816d0c815469b0a205b5c
KamyarTaher <61198701+KamyarTaher@users.noreply.github.com>
2024-02-15 20:38:27 +0100

    width fix

--- src/components/trailer/index.tsx
+++ src/components/trailer/index.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export const Trailer = () => {
   return (
     <div className="flex flex-col justify-center items-center relative mb-24 text-black">
@@ -9,8 +7,8 @@ export const Trailer = () => {
       <div style={{ zIndex: -1 }} className="absolute flex justify-center items-center w-full h-full">
         <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
       </div>
-      <div style={{ height: "325px", width: "560px" }}>
-        <blockquote className="twitter-tweet" data-media-max-width="560" >
+      <div style={{ height: "325px", width: "100%", display: "flex", justifyContent: "center" }}>
+        <blockquote className="twitter-tweet" data-media-max-width="560"  >
           <a href="https://twitter.com/MetacubeGames/status/1714343398247682273?ref_src=twsrc%5Etfw"></a>
         </blockquote>
         <script async src="https://platform.twitter.com/widgets.js" />
