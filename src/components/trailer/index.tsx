af4a11ee3cea78c25ed95963270bd098b5e68fb7
KamyarTaher <61198701+KamyarTaher@users.noreply.github.com>
2024-02-24 00:40:27 +0100

    perf opt

--- src/components/trailer/index.tsx
+++ src/components/trailer/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import { TwitterWidget } from "./TwitterWidget";
+import { TwitterWidget } from "./TwitterWidget";
 
 const Trailer = () => {
   return (
@@ -11,16 +11,14 @@ const Trailer = () => {
         <div className="spinner-container absolute flex justify-center items-center w-full h-full">
           <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
         </div>
-
         <div className="flex justify-center items-center w-full" style={{ height: "325px" }}>
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/WT58alm2_a0?si=GqlsadqotzFWxPEQ" loading="lazy" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-
-          {/* <blockquote className="twitter-tweet" data-media-max-width="560" >
+          {/* Ensure the tweet embed is accessible and responsive */}
+          <blockquote className="twitter-tweet" data-media-max-width="560" >
             <a href="https://twitter.com/MetacubeGames/status/1714343398247682273?ref_src=twsrc%5Etfw"></a>
-          </blockquote>*/}
+          </blockquote>
         </div>
       </div>
-      {/* <TwitterWidget /> */}
+      <TwitterWidget />
     </>
   );
 };
