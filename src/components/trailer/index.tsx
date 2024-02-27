5427a0c4db5360d301b75b37c6ddef9f9449c7c6
KamyarTaher <61198701+KamyarTaher@users.noreply.github.com>
2024-02-27 21:58:19 +0100

    clean

--- src/components/trailer/index.tsx
+++ src/components/trailer/index.tsx
@@ -8,9 +8,8 @@ const Trailer = () => {
         <h1 className="text-3xl font-medium tracking-widest uppercase text-center mb-4">
           Watch the Trailer
         </h1>
-
         <div className="flex justify-center items-center w-full" >
-          <YouTubeEmbed videoid="WT58alm2_a0" width={560} params="controls=0" />
+          <YouTubeEmbed videoid="WT58alm2_a0" width={560} params="controls=0" playlabel="Metacube trailer" />
         </div>
       </div>
     </>
