c2fc9cdcd066d26e3ae066869a9a80334b63139b
Kamyar Taher <61198701+KamyarTaher@users.noreply.github.com>
2025-03-24 19:02:43 +0100

    update team

--- src/components/team/index.tsx
+++ src/components/team/index.tsx
@@ -5,50 +5,40 @@ const people = [
     id: 1,
     name: "Kamyar Taher",
     designation: "Web Game Engineer",
-    image:
-      "/ppKam.webp",
-    link: "https://ch.linkedin.com/in/kamyar-taher-4380b614a"
+    image: "/ppKam.webp",
+    link: "https://ch.linkedin.com/in/kamyar-taher-4380b614a",
   },
   {
     id: 2,
     name: "Bastien Faivre",
     designation: "Game Systems Engineer",
-    image:
-      "/ppBast.webp",
-    link: "https://ch.linkedin.com/in/bastienfaivre"
+    image: "/ppBast.webp",
+    link: "https://ch.linkedin.com/in/bastienfaivre",
   },
   {
     id: 3,
     name: "Nils Delage",
     designation: "Full-Stack Developer",
-    image:
-      "/ppNils.webp",
-    link: "https://ch.linkedin.com/in/nils-delage-934a67239"
+    image: "/ppNils.webp",
+    link: "https://ch.linkedin.com/in/nils-delage-934a67239",
   },
   {
     id: 4,
-    name: "Damien Elledge",
-    designation: "Marketing Strategist",
-    image:
-      "/ppDam.webp",
-    link: "https://fr.linkedin.com/in/damien-elledge-30740058"
-  },
-  {
-    id: 5,
     name: "Hartom",
     designation: "Community Manager",
-    image:
-      "/ppHartom.webp",
-    link: "https://twitter.com/hartomicfundz"
+    image: "/ppHartom.webp",
+    link: "https://x.com/TheRealHartom",
   },
-
 ];
 
 export default function Team() {
   return (
     <section aria-labelledby="team-heading">
       <div className="flex flex-row items-center justify-center w-full">
-        <h1 id="team-heading" className="text-4xl font-medium tracking-widest uppercase text-center text-white">
+        <h1
+          id="team-heading"
+          className="text-4xl font-medium tracking-widest uppercase text-center text-white"
+        >
           Our Team
         </h1>
       </div>
